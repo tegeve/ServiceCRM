4830b5c8a2197e6743560075bd5d25be4d7ca112
tegeve <97911489+tegeve@users.noreply.github.com>
2022-10-16 15:17:32 +0300

    edit

--- servicemanagement/settings.py
+++ servicemanagement/settings.py
@@ -119,8 +119,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    STATIC_DIR,
-]
+    STATIC_DI
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
