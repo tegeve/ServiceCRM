cb855dab956941ec4345ff2861fed4c47fe41e53
tegeve <97911489+tegeve@users.noreply.github.com>
2022-10-16 15:43:15 +0300

    edit

--- servicemanagement/settings.py
+++ servicemanagement/settings.py
@@ -121,6 +121,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
